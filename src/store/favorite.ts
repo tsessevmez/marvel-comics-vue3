d61cfdc561b5ee0430488936304905dd84b09969
tahaeren1 <tahaeren1997@gmail.com>
2023-06-15 00:43:45 +0300

    updated

--- src/store/favorite.ts
+++ src/store/favorite.ts
@@ -1,32 +1,39 @@
 import { Module } from "vuex";
 
+// Favori öğelerin durumunu tanımlayan arayüz
 interface FavoriteState {
-  favoriteItems: any[];
+  favoriteItems: any[]; // Favori öğelerin dizi olarak tutulduğu alan
 }
 
+// Vuex modülünü tanımlayan favori modülü
 const favoriteModule: Module<FavoriteState, any> = {
   state: {
-    favoriteItems: JSON.parse(localStorage.getItem("favorites") || "[]"),
+    favoriteItems: JSON.parse(localStorage.getItem("favorites") || "[]"), // Başlangıç durumu, localStorage'dan favori öğeleri getiriyor
   },
   mutations: {
+    // Favorilere öğe eklemek için mutasyon fonksiyonu
     addToFavorites(state, item) {
-      state.favoriteItems.push(item);
-      localStorage.setItem("favorites", JSON.stringify(state.favoriteItems));
+      state.favoriteItems.push(item); // Öğeyi favori öğeler dizisine ekler
+      localStorage.setItem("favorites", JSON.stringify(state.favoriteItems)); // Güncellenmiş favori öğelerini localStorage'a kaydeder
     },
+    // Favorilerden öğe çıkarmak için mutasyon fonksiyonu
     removeFromFavorites(state, item) {
       state.favoriteItems = state.favoriteItems.filter(
         (favorite) => favorite.id !== item.id
-      );
-      localStorage.setItem("favorites", JSON.stringify(state.favoriteItems));
+      ); // Öğeyi favori öğeler dizisinden filtreler
+      localStorage.setItem("favorites", JSON.stringify(state.favoriteItems)); // Güncellenmiş favori öğelerini localStorage'a kaydeder
     },
   },
   getters: {
+    // Favori öğe sayısını hesaplayan getter fonksiyonu
     favoriteItemCount(state) {
       return state.favoriteItems.length;
     },
+    // Tüm favori öğelerini döndüren getter fonksiyonu
     favoriteItemData(state) {
       return state.favoriteItems;
     },
+    // Belirli bir öğenin favorilerde olup olmadığını kontrol eden getter fonksiyonu
     isItemInFavorites: (state) => (item: any) => {
       return state.favoriteItems.some((favorite) => favorite.id === item.id);
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,4 +12,9 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import MarvelComics from "./apps/marvel-comics/ComicsComponent.vue";
 import MainComponent from "./apps/main-page/MainComponent.vue";
+
+const ComicsComponent = {
+  // Bileşen seçenekleri
+  // ...
+};
 </script>
