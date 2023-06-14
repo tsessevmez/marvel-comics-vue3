b66381c5b1dc994c8d5ff37c2606f0778012d243
tahaeren1 <tahaeren1997@gmail.com>
2023-06-15 00:50:56 +0300

    updated

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from "vuex";
-import comicsModule from "./comics";
 import favoriteModule from "./favorite";
 
 export default createStore({
   modules: {
-    comicsModule,
     favorites: favoriteModule,
   },
 });
